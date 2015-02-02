43fd263165d86b43fe2ada7ad4dd4575991fe8db
Peter Poole <peter.j.poole@gmail.com>
2015-02-02 14:33:24 -0500

    Add other cats routes

--- bin/server.rb
+++ bin/server.rb
@@ -1,10 +1,11 @@
 require 'webrick'
-require_relative '../lib/controller_base'
 require_relative '../lib/router'
 
 router = Router.new
 router.draw do
-  get Regexp.new("^/cats$"), Cats2Controller, :index
+  get Regexp.new("^/cats$"), CatsController, :index
+  get Regexp.new("^/cats/new$"), CatsController, :new
+  post Regexp.new("^/cats$"), CatsController, :create
   get Regexp.new("^/cats/(?<cat_id>\\d+)/statuses$"), StatusesController, :index
 end
 
