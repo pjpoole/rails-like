326efe4effb776dc166471ce914174bbd5c5e8b5
Peter Poole <peter.j.poole@gmail.com>
2015-02-02 14:33:46 -0500

    Add cats and statuses controllers

---
+++ controllers/cats_controller.rb
@@ -0,0 +1,46 @@
+require_relative '../lib/controller_base'
+
+class Cat
+  attr_reader :name, :owner
+
+  def self.all
+    @cat ||= []
+  end
+
+  def initialize(params = {})
+    params ||= {}
+    @name, @owner = params["name"], params["owner"]
+  end
+
+  def save
+    return false unless @name.present? && @owner.present?
+
+    Cat.all << self
+    true
+  end
+
+  def inspect
+    { name: name, owner: owner }.inspect
+  end
+end
+
+class CatsController < ControllerBase
+  def create
+    @cat = Cat.new(params["cat"])
+    if @cat.save
+      redirect_to("/cats")
+    else
+      render :new
+    end
+  end
+
+  def index
+    @cats = Cat.all
+    render :index
+  end
+
+  def new
+    @cat = Cat.new
+    render :new
+  end
+end
---
+++ controllers/statuses_controller.rb
@@ -0,0 +1,17 @@
+require_relative '../lib/controller_base'
+
+$statuses = [
+  { id: 1, cat_id: 1, text: "Curie loves string!" },
+  { id: 2, cat_id: 2, text: "Markov is mighty!" },
+  { id: 3, cat_id: 1, text: "Curie is cool!" }
+]
+
+class StatusesController < ControllerBase
+  def index
+    statuses = $statuses.select do |s|
+      s[:cat_id] == Integer(params[:cat_id])
+    end
+
+    render_content(statuses.to_s, "text/text")
+  end
+end
